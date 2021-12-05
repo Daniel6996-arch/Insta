b05efce537548fbff0adccee21472cfc7bb001d8
Daniel6996-arch <nyutu.daniel@student.moringaschool.com>
2021-12-05 12:58:25 +0300

    add models and connect to database

--- instagram/urls.py
+++ instagram/urls.py
@@ -7,6 +7,3 @@ urlpatterns=[
     url('^$',views.index,name = 'index'),
 ]
 
-if settings.DEBUG:
-    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
