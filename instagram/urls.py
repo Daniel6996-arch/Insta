161d81cef423b52722d9f7a37daa80b777c0b072
Daniel6996-arch <nyutu.daniel@student.moringaschool.com>
2021-12-08 00:32:01 +0300

    edit likes functionality

--- instagram/urls.py
+++ instagram/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.static import static
 from django.conf.urls import url
 from django.urls import path
 from . import views
-from .views import ImageListView, ImageDetailView, ImageEditView, ImageDeleteView, ProfileView, ProfileEditView, AddFollower, RemoveFollower
+from .views import ImageListView, ImageDetailView, ImageEditView, ImageDeleteView, ProfileView, ProfileEditView, AddFollower, RemoveFollower, AddLike, Dislike
 
 urlpatterns=[
     url('^$', views.landing, name='landing'),
@@ -12,6 +12,8 @@ urlpatterns=[
     path('post/<int:pk>', ImageDetailView.as_view(), name = 'image-detail'),
     path('post/edit/<int:pk>', ImageEditView.as_view(), name = 'image-edit'),
     path('post/delete/<int:pk>', ImageDeleteView.as_view(), name = 'image-delete'),
+    path('post/<int:pk>/like', AddLike.as_view(), name = 'like'),
+    path('post/<int:pk>/dislike', Dislike.as_view(), name = 'dislike'),
     path('^profile/edit/<int:pk>/', ProfileEditView.as_view(), name = 'profile-edit'),
     path('^profile/<int:pk>/followers/add', AddFollower.as_view(), name = 'add-follower'),
     path('^profile/<int:pk>/followers/remove', RemoveFollower.as_view(), name = 'remove-follower'),
